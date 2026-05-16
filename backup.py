75c279361053ac5168a8b2845a4c24cf2a2b3e38
diccob <101421024+Diccob@users.noreply.github.com>
2026-05-16 13:07:34 +0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,1 +1,51 @@
+import os
+import shutil
+from datetime import datetime
 
+# =========================
+# ПУТИ
+# =========================
+
+DB_PATH = "/app/shared/smokes.db"
+
+BACKUP_DIR = "/app/shared/backups"
+
+# =========================
+# СОЗДАНИЕ ПАПКИ
+# =========================
+
+os.makedirs(BACKUP_DIR, exist_ok=True)
+
+# =========================
+# ИМЯ БЭКАПА
+# =========================
+
+timestamp = datetime.now().strftime(
+    "%Y-%m-%d_%H-%M-%S"
+)
+
+backup_name = f"smokes_backup_{timestamp}.db"
+
+backup_path = os.path.join(
+    BACKUP_DIR,
+    backup_name
+)
+
+# =========================
+# СОЗДАНИЕ БЭКАПА
+# =========================
+
+try:
+
+    shutil.copy2(DB_PATH, backup_path)
+
+    print(
+        f"✅ Backup создан: "
+        f"{backup_path}"
+    )
+
+except Exception as e:
+
+    print(
+        f"❌ Ошибка backup: {e}"
+    )
