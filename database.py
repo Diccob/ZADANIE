e1bd3a79b497c00e8b4a94451e3802760c71400e
diccob <101421024+Diccob@users.noreply.github.com>
2026-05-16 07:31:50 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+import aiosqlite
+
+DB_NAME = "smoke.db"
+
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS smokes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            date TEXT
+        )
+        """)
+        await db.commit()
