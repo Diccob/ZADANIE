d323248771b2e9cbe664e8c4f4723f4de8a1e8d0
diccob <101421024+Diccob@users.noreply.github.com>
2026-05-16 14:00:06 +0500

    Update database.py

--- database.py
+++ database.py
@@ -7,12 +7,14 @@ async def init_db():
 
     async with aiosqlite.connect(DB_NAME) as db:
 
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS smokes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            date TEXT
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS smokes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                date TEXT
+            )
+            """
         )
-        """)
 
         await db.commit()
